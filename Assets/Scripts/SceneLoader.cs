42b89b98362e8ef63a6cd271fdd5dfe4d762bb27
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-06-10 17:08:48 +0000

    Pedro adds a method LoadStartScene() that can be seen as a hard reset or scene loaded from index 0. We can now navigate through all scenes without crashes.

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -10,4 +10,8 @@ public class SceneLoader : MonoBehaviour
         SceneManager.LoadScene(currentSceneIndex + 1);
     }
 
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
